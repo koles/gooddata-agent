17e6f702d78012b333495df6fa4779aaec5ba795
Pavel Kolesnikov <pavel@gooddata.com>
2013-06-20 19:11:01 -0700

    HTTP User Agent is "GoodData Agent/VERSION" now
    (it was GoodData CL/1.2.68 before!)

--- src/main/java/com/gooddata/agent/api/GdcRESTApiWrapper.java
+++ src/main/java/com/gooddata/agent/api/GdcRESTApiWrapper.java
@@ -3240,7 +3240,7 @@ public class GdcRESTApiWrapper {
         request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
         request.setRequestHeader("Accept", "application/json");
         request.setRequestHeader("Accept-Charset", "utf-u");
-        request.setRequestHeader("User-Agent", "GoodData CL/1.2.68");
+        request.setRequestHeader("User-Agent", "GoodData Agent/0.4");
         request.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
         return request;
     }
